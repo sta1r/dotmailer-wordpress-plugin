bc679eed0d73aebdb33422dd87f641a13a199b24
Crystal Preston <Crystal.Preston@dotmailer.com>
2018-12-19 16:54:34 +0000

    added dotmailer icon

--- src/block/block.js
+++ src/block/block.js
@@ -9,21 +9,29 @@ const Components = wp.components;
 var $ = require('jQuery');
 import { SelectControl } from '@wordpress/components';
 import { withState } from '@wordpress/compose';
+var Fragment = wp.element.Fragment,
+    RichText = wp.editor.RichText,
+    BlockControls = wp.editor.BlockControls,
+	AlignmentToolbar = wp.editor.AlignmentToolbar;
+	
+	const BasicSvg = () =>
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M8 0a8 8 0 1 0 8 8 8 8 0 0 0-8-8m0 13.21A5.21 5.21 0 1 1 13.21 8 5.22 5.22 0 0 1 8 13.21" fill="#80c342"/><path d="M8 2.79A5.21 5.21 0 1 0 13.21 8 5.22 5.22 0 0 0 8 2.79z" fill="#fff"/><path d="M10.05 8A2.05 2.05 0 1 1 8 6a2.05 2.05 0 0 1 2.05 2" fill="#ec008b"/></svg>
 
 registerBlockType( 'cgb/block-dd-block', {
 	title: __( 'dd-block - Dotdigital Block' ), 
-	icon: 'shield', 
+	icon: BasicSvg, 
 	category: 'common',
 	keywords: [
 		__( 'dd-block — Dotdigital Block' ),
 		__( 'CGB Example' ),
-		__( 'create-guten-block' ),
+		__( 'create-guten-block' )
 	],
 	attributes: {
 		id: {
 			type: 'string',
 		}
-	},
+    },
+
     edit: function( props ) {
         var id = props.attributes.id || '',
 			focus = props.focus;
@@ -49,7 +57,7 @@ registerBlockType( 'cgb/block-dd-block', {
 				React.createElement(
 					'div',
 					{ 'id': 'survey-container' }
-				)
+				, Fragment)
 			);
 			if(props.attributes.id) {
 				$('#survey-container').html('<iframe frameBorder="0" src="'+props.attributes.id+'"></iframe>');
@@ -62,5 +70,5 @@ registerBlockType( 'cgb/block-dd-block', {
 		return (
 			<iframe frameBorder="0" src={props.attributes.id}></iframe>
 		);
-	},
+	}
 } );
