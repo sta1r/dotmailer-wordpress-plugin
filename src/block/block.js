89bd9712a2e4e88e58ab2f4c4c627bdbc56ede2a
Crystal Preston <crystal.preston@dotmailer.com>
2018-12-21 11:19:23 +0000

    set better static data & fixed icon

--- src/block/block.js
+++ src/block/block.js
@@ -48,7 +48,7 @@ var Fragment = wp.element.Fragment,
 	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M8 0a8 8 0 1 0 8 8 8 8 0 0 0-8-8m0 13.21A5.21 5.21 0 1 1 13.21 8 5.22 5.22 0 0 1 8 13.21" fill="#80c342"/><path d="M8 2.79A5.21 5.21 0 1 0 13.21 8 5.22 5.22 0 0 0 8 2.79z" fill="#fff"/><path d="M10.05 8A2.05 2.05 0 1 1 8 6a2.05 2.05 0 0 1 2.05 2" fill="#ec008b"/></svg>
 
 registerBlockType( 'cgb/block-dd-block', {
-    title: __( 'Dotdigital Survey' ),
+	title: __( 'dd-block - Dotdigital Block' ), 
 	icon: BasicSvg, 
 	category: 'common',
 	keywords: [
@@ -69,7 +69,7 @@ registerBlockType( 'cgb/block-dd-block', {
 
 		dotsurvey.surveys = dotsurvey.getSurveyList();
 		
-		dotsurvey.surveys = [{"label": "-- Select survey --", "value":-1}, {"label":"Crystal Test Survey","value":"https://r1.dotmailer-surveys.com/204mlv7a-f42tvfb1"},{"label":"Cats or dogs?","value":"https://r1.dotmailer-surveys.com/204mlv7a-e12tvh14"},{"label":"Landing page embedded survey","value":"https://r1.dotmailer-surveys.com/204mlv7a-8f3isi05"},{"label":"Landing page embedded survey","value":"https://r1.dotmailer-surveys.com/204mlv7a-503isjd1"},{"label":"Landing page embedded survey","value":"https://r1.dotmailer-surveys.com/204mlv7a-f33iss54"},{"label":"Landing page embedded survey","value":"https://r1.dotmailer-surveys.com/204mlv7a-e53izob2"},{"label":"survey 1","value":"https://r1.dotmailer-surveys.com/204mlv7a-9e3jrsda"},{"label":"survey 2","value":"https://r1.dotmailer-surveys.com/204mlv7a-8a3jrt2e"}]
+		dotsurvey.surveys = [{"label": "-- Select survey --", "value":-1}, {"label":"SnowYo survey","value":"https://r1.dotmailer-surveys.com/204mlv7a-a03jwe7f"},{"label":"Cats or dogs?","value":"https://r1.dotmailer-surveys.com/204mlv7a-e12tvh14"}]
 		var retval = [];
 
 
@@ -97,7 +97,7 @@ registerBlockType( 'cgb/block-dd-block', {
 			);
 			if(props.attributes.id) {
 				let valid = (props.attributes.id.match(validUrlRegEx) !== null);
-				let html = (valid) ? '<iframe onLoad="console.log(this.contentWindow.document.body.offsetHeight)" frameBorder="0" src="'+props.attributes.id+'"></iframe>': '';
+				let html = (valid) ? '<iframe frameBorder="0" src="'+props.attributes.id+'"></iframe>': '';
 				$('#survey-container').html(html);
 			}
         return retval;
